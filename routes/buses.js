dd51bd067041df81f1c2b7c49ef231ee46741e63
Lihini Wimalasooriya <lihinisupunikaw@gmail.com>
2024-12-30 08:58:41 +0530

    operator access to delete bus request (#21)

--- routes/buses.js
+++ routes/buses.js
@@ -18,7 +18,7 @@ router.get("/defaultTrips", authenticate(["admin", "operator"]), getAllTrips);
 router.post("/", authenticate(["admin", "operator"]), addBus);
 router.put("/:id", authenticate(["admin", "operator"]), updateBus);
 router.delete("/defaultTrips/:id", authenticate(["admin", "operator"]), deleteDefaultTrip);
-router.delete("/:id", authenticate(["admin"]), deleteBus);
+router.delete("/:id", authenticate(["admin", "operator"]), deleteBus);
 router.get("/", authenticate(["admin", "operator", "commuter"]), getBuses);
 router.get("/:id", authenticate(["admin", "operator", "commuter"]), getBusById);
 
