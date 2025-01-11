56456469425c1e112fd59cacef8b0738e51629b8
lihiniswimalasooriya <lihinisupunikaw@gmail.com>
2025-01-11 23:54:40 +0530

    checkdep

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const authRoutes = require("./routes/auth");
 const routeRoutes = require("./routes/routes");
 const busRoutes = require("./routes/buses");
 const reservationRoutes = require("./routes/reservations");
-const setupSwagger = require("./swagger");
+// const setupSwagger = require("./swagger");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
 
 dotenv.config();
 const app = express();
@@ -17,7 +20,48 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-setupSwagger(app);
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Seat Booking API",
+        version: "1.0.0",
+        description: "API documentation for the Seat Booking application",
+      },
+      servers: [
+        {
+          url: "https://lw-seat-booking-api.vercel.app/"
+          // url: "http://localhost:5000/" ,
+        },
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+    },
+    apis: ["./routes/auth.js", "./controllers/*.js"],
+  };
+
+const specs = swaggerJsDoc(options);
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+);
+
+// setupSwagger(app);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Bus Reservation System API!");
--- swagger.js
+++ swagger.js
@@ -1,56 +1,47 @@
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+// const swaggerJsdoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+// const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Seat Booking API",
-      version: "1.0.0",
-      description: "API documentation for the Seat Booking application",
-    },
-    servers: [
-      {
-        url: "https://lw-seat-booking-api.vercel.app/" ,
-        // url: "http://localhost:5000/" ,
-      },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
-  },
-  apis: ["./routes/auth.js", "./controllers/*.js"],
-};
+// const swaggerOptions = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Seat Booking API",
+//       version: "1.0.0",
+//       description: "API documentation for the Seat Booking application",
+//     },
+//     servers: [
+//       {
+//         // url: "https://lw-seat-booking-api.vercel.app/" ,
+//         url: "http://localhost:5000/" ,
+//       },
+//     ],
+//     components: {
+//       securitySchemes: {
+//         bearerAuth: {
+//           type: "http",
+//           scheme: "bearer",
+//           bearerFormat: "JWT",
+//         },
+//       },
+//     },
+//     security: [
+//       {
+//         bearerAuth: [],
+//       },
+//     ],
+//   },
+//   apis: ["./routes/auth.js", "./controllers/*.js"],
+// };
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
+// const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
-const setupSwagger = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-    customCssUrl: CSS_URL 
-    // swaggerOptions: {
-    //     url: "/swagger.json",
-    //     dom_id: "#swagger-ui",
-    //   },
-  }));
-
-  app.get('/swagger.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-  });
-};
+// const setupSwagger = (app) => {
+//   app.use("/api-docs",
+//     swaggerUi.serve,
+//     swaggerUi.setup(swaggerSpec, {customCssUrl: CSS_URL }));
+// };
 
   
-module.exports = setupSwagger;
+// module.exports = setupSwagger;
